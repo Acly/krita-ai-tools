4d7de14fc2a407901166825f4f6dd1a7020d4fe5
Acly <aclysia@gmail.com>
2025-07-21 13:39:02 +0200

    Add /usr/bin of AppImage root to LD_LIBRARY_PATH to find krita libs

--- python/extension.py
+++ python/extension.py
@@ -4,6 +4,14 @@ import sys
 from krita import Extension, Krita
 from pathlib import Path
 
+if sys.platform in ["win32", "cygwin", "msys"]:
+    platform = "windows"
+elif sys.platform == "linux":
+    platform = "linux"
+elif sys.platform == "darwin":
+    platform = "macos"
+else:
+    raise RuntimeError(f"Unsupported platform: {sys.platform}")
 
 class VisionMLExtension(Extension):
     """Loader for Vision ML tools and filters.
@@ -15,23 +23,23 @@ class VisionMLExtension(Extension):
     def __init__(self, parent):
         super().__init__(parent)
 
-        if sys.platform in ["win32", "cygwin", "msys"]:
-            ext = ".dll"
-        elif sys.platform == "linux":
-            ext = ".so"
-        elif sys.platform == "darwin":
-            ext = ".dylib"
-        else:
-            raise RuntimeError(f"Unsupported platform: {sys.platform}")
-
+        ext = {"windows": ".dll", "linux": ".so", "macos": ".dylib"}[platform]
         lib_dir = Path(__file__).parent / "lib"
         lib_file = lib_dir / f"kritavisionml{ext}"
 
         executable_dir = Path(sys.executable).parent
-        env_path = os.environ.get("PATH", "")
-        os.environ["PATH"] = (
-            f"{executable_dir}{os.pathsep}{lib_dir.resolve()}{os.pathsep}{env_path}"
-        )
+        bin_paths = []
+        if platform == "windows":
+            bin_paths = [lib_dir, executable_dir]
+        env_path = _env_add_path("PATH", *bin_paths)
+
+        ld_paths = []
+        if platform == "linux":
+            ld_paths.append(lib_dir.resolve())
+        if platform == "linux" and "APPDIR" in os.environ: # for AppImage
+            ld_paths.append(Path(os.environ["APPDIR"]) / "usr" / "lib")
+        ld_path = _env_add_path("LD_LIBRARY_PATH", *ld_paths)
+
         try:
             lib = ctypes.CDLL(str(lib_file.resolve()))
             lib.load_vision_ml_plugin()
@@ -47,7 +55,8 @@ class VisionMLExtension(Extension):
                 f"Failed to load VisionML library from {lib_file}: {e}{deps}"
             )
         finally:
-            os.environ["PATH"] = env_path
+            _restore_env("PATH", env_path)
+            _restore_env("LD_LIBRARY_PATH", ld_path)
 
     def setup(self):
         pass
@@ -60,3 +69,16 @@ class VisionMLExtension(Extension):
 
 
 Krita.instance().addExtension(VisionMLExtension(Krita.instance()))
+
+
+def _env_add_path(var: str, *paths: str | Path):
+    prev = os.environ.get(var, "")
+    if not paths:
+        return prev
+    paths = os.pathsep.join(str(p) for p in paths)
+    os.environ[var] = f"{paths}{os.pathsep}{prev}" if prev else paths
+    return prev
+
+def _restore_env(var: str, value: str):
+    if value:
+        os.environ[var] = value
